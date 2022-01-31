b1af253c5cc5c969a83220886b2c2c5acf0786b8
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-31 14:43:18 -0800

    commit tree 619e9639089fa4b01ad81c2e4d7ad9ad729ef94b

--- 01_intro_to_react/toy_tale_react/src/App.js
+++ 01_intro_to_react/toy_tale_react/src/App.js
@@ -11,51 +11,57 @@
       - Import and store data from the "toys.js" file
       - Create a "components" subdirectory + "ToyCard.js" file
 */
+function ToyCard(props) {
+  return (
+
+  )
+}
+
 
 function App() {
   return (
     <div>
       <header className="App-header">
-      <div id="toy-header">
-      <img
-        src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
-        alt="toy-header"
-      />
-    </div>
+        <div id="toy-header">
+          <img
+          src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+          alt="toy-header"
+          />
+        </div>
 
-    <div class="container">
-      <form class="add-toy-form">
-        <h3>Create a toy!</h3>
+        <div classNamelearn="container">
+          <form className="add-toy-form">
+            <h3>Create a toy!</h3>
 
-        <input
-          type="text"
-          name="name"
-          value=""
-          placeholder="Enter a toy's name..."
-          class="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="image"
-          value=""
-          placeholder="Enter a toy's image URL..."
-          class="input-text"
-        />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create Toy"
-          class="submit"
-        />
-      </form>
-    </div>
-    {/* <p style="text-align:center">
-      Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
-    </p> */}
-    </header>
-    <div id="toy-collection"></div>
+            <input
+              type="text"
+              name="name"
+              value=""
+              placeholder="Enter a toy's name..."
+              className="input-text"
+            />
+            <br />
+            <input
+              type="text"
+              name="image"
+              value=""
+              placeholder="Enter a toy's image URL..."
+              className="input-text"
+            />
+            <br />
+            <input
+              type="submit"
+              name="submit"
+              value="Create Toy"
+              className="submit"
+            />
+          </form>
+        </div>
+        {/* <p style="text-align:center">
+        Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
+        </p> */}
+      </header>
+      <div id="toy-collection"></div>
     </div>
   );
 }
